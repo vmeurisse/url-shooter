e9654d63137db3cbe404722d0a6e56ccd32db4bd
Vincent Meurisse <dev@meurisse.org>
2014-05-19 10:47:51 +0200

    Auto insert/remove params inputs when needed

--- data/sidebar.js
+++ data/sidebar.js
@@ -25,6 +25,68 @@ up.onUrl = function(url) {
 	this.displayParams('postInputs', url.postData);
 };
 
+up.lastInputLine = {};
+up.getLastInputLine = function(type) {
+	if (this.lastInputLine[type]) return this.lastInputLine[type];
+	
+	var inputs = document.getElementById(type).getElementsByTagName('input');
+	this.lastInputLine[type] = [inputs[inputs.length - 2], inputs[inputs.length - 1]];
+	return this.lastInputLine[type];
+};
+
+up.previousInputLine = {};
+up.getPreviousInputLine = function(type) {
+	if (this.previousInputLine[type] !== undefined) return this.previousInputLine[type];
+	
+	var inputs = document.getElementById(type).getElementsByTagName('input');
+	if (inputs.length >= 4) {
+		this.previousInputLine[type] = [inputs[inputs.length - 4], inputs[inputs.length - 3]];
+	} else {
+		this.previousInputLine[type] = null;
+	}
+	return this.previousInputLine[type];
+};
+
+up.onInput = function(type) {
+	var inputs = up.getLastInputLine(type);
+	if (inputs[0].value || inputs[1].value) {
+		up.insertLastLine(type);
+	} else {
+		while (1) {
+			inputs = up.getPreviousInputLine(type);
+			if (!inputs || inputs[0].value || inputs[1].value) break;
+			up.deleteLastLine(type);
+		}
+	}
+};
+
+up.insertLastLine = function(type, dom) {
+	if (!dom) dom = document.getElementById(type);
+	
+	dom.insertAdjacentHTML('beforeend', supplant(this.tpl, {key: '', value: ''}));
+	delete this.previousInputLine[type];
+	delete this.lastInputLine[type];
+};
+
+up.deleteLastLine = function(type, dom) {
+	if (!dom) dom = document.getElementById(type);
+	
+	var inputs = this.getLastInputLine(type);
+	var focused = document.activeElement;
+	if (focused === inputs[0]) focused = 0;
+	else if (focused === inputs[1]) focused = 1;
+	else focused = null;
+	
+	dom.removeChild(dom.lastElementChild);
+	this.lastInputLine[type] = this.previousInputLine[type];
+	delete this.previousInputLine[type];
+	
+	if (focused === 0 || focused === 1) {
+		inputs = this.getLastInputLine(type);
+		inputs[focused].focus();
+	}
+};
+
 up.displayParams = function(id, string) {
 	var dom = document.getElementById(id);
 	dom.innerHTML = '';
@@ -39,7 +101,7 @@ up.displayParams = function(id, string) {
 			dom.insertAdjacentHTML('beforeend', supplant(this.tpl, {key: param[0], value: param[1]}));
 		}
 	}
-	dom.insertAdjacentHTML('beforeend', supplant(this.tpl, {key: '', value: ''}));
+	up.insertLastLine(id, dom);
 };
 
 up.readParams = function(id) {
